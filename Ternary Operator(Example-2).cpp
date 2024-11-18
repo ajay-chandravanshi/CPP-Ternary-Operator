b09cb5fe7703acd76edbee512ee0f1cbfd59f369
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-11-18 23:05:51 +0530

    first commit

---
+++ Ternary Operator(Example-2).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number=8;
+    (number%2==0)?cout<<"even no.":cout<<"odd";
+   
+
+}
---
+++ Ternary Operator(Example-3).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1,number2;
+    cout<<"enter two numbers=";
+    cin>>number1>>number2;
+    (number1>number2)?cout<<number1<<" is graeter":cout<<number2<<" is greater";
+   
+
+}
